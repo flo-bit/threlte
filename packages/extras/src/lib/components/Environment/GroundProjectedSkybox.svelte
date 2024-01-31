45cfa801e3499e4c1c7c88f887a7738f91bfe0c8
Florian <45694132+flo-bit@users.noreply.github.com>
2024-01-31 13:16:10 +0100

    try dynamic import

--- packages/extras/src/lib/components/Environment/GroundProjectedSkybox.svelte
+++ packages/extras/src/lib/components/Environment/GroundProjectedSkybox.svelte
@@ -1,15 +1,33 @@
 <script lang="ts">
   import { T } from '@threlte/core'
   import type { Texture } from 'three'
-  import { GroundProjectedSkybox } from 'three/examples/jsm/objects/GroundProjectedSkybox'
+  import { revision } from '../../lib/revision'
+  import { writable } from 'svelte/store'
 
   export let envMap: Texture
+
+  // Define a store to hold the dynamically imported component
+  const skyboxComponent = writable<unknown>(null)
+
+  ;(async () => {
+    const module = await import(
+      '../../../../node_modules/three/examples/jsm/objects/' +
+        (revision >= 161 ? 'GroundedSkybox' : 'GroundProjectedSkybox') +
+        '.js'
+    )
+		console.log(module)
+    skyboxComponent.set(module.GroundProjectedSkybox)
+  })()
+
+	$:console.log($skyboxComponent)
 </script>
 
 {#if envMap}
-  <T
-    is={GroundProjectedSkybox}
-    args={[envMap]}
-    {...$$restProps}
-  />
+  {#await $skyboxComponent then component}
+    <T
+      is={component}
+      args={[envMap]}
+      {...$$restProps}
+    />
+  {/await}
 {/if}
